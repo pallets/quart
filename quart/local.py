f3bb697d68783a854d2b12053736cd12aee8ec76
pgjones <philip.graham.jones@googlemail.com>
2017-05-31 20:08:00 +0100

    Change _get_proxied to _get_current_object
    It is a common pattern to use _get_current_object when spawning a
    thread in Flask, so the name should be consistent (analogue is
    spawning a task in Quart).

--- quart/local.py
+++ quart/local.py
@@ -73,95 +73,95 @@ class LocalProxy:
         object.__setattr__(self, '__LocalProxy_local', local)
         object.__setattr__(self, '__wrapped__', local)
 
-    def _get_proxied(self) -> Any:
+    def _get_current_object(self) -> Any:
         return object.__getattribute__(self, '__LocalProxy_local')()
 
     @property
     def __dict__(self) -> Dict[str, Any]:  # type: ignore
         try:
-            return self._get_proxied().__dict__
+            return self._get_current_object().__dict__
         except RuntimeError:
             raise AttributeError('__dict__')
 
     def __repr__(self) -> str:
         try:
-            obj = self._get_proxied()
+            obj = self._get_current_object()
         except RuntimeError:
             return '<%s unbound>' % self.__class__.__name__
         return repr(obj)
 
     def __bool__(self) -> bool:
         try:
-            return bool(self._get_proxied())
+            return bool(self._get_current_object())
         except RuntimeError:
             return False
 
     def __dir__(self) -> Any:
         try:
-            return dir(self._get_proxied())
+            return dir(self._get_current_object())
         except RuntimeError:
             return []
 
     def __getattr__(self, name: Any) -> Any:
         if name == '__members__':
-            return dir(self._get_proxied())
-        return getattr(self._get_proxied(), name)
+            return dir(self._get_current_object())
+        return getattr(self._get_current_object(), name)
 
     def __setitem__(self, key: Any, value: Any) -> Any:
-        self._get_proxied()[key] = value
+        self._get_current_object()[key] = value
 
     def __delitem__(self, key: Any) -> Any:
-        del self._get_proxied()[key]
+        del self._get_current_object()[key]
 
-    __setattr__ = lambda x, n, v: setattr(x._get_proxied(), n, v)  # type: ignore # noqa
-    __delattr__ = lambda x, n: delattr(x._get_proxied(), n)  # type: ignore # noqa
-    __str__ = lambda x: str(x._get_proxied())  # type: ignore # noqa
-    __lt__ = lambda x, o: x._get_proxied() < o  # type: ignore # noqa
-    __le__ = lambda x, o: x._get_proxied() <= o  # type: ignore # noqa
-    __eq__ = lambda x, o: x._get_proxied() == o  # type: ignore # noqa
-    __ne__ = lambda x, o: x._get_proxied() != o  # type: ignore # noqa
-    __gt__ = lambda x, o: x._get_proxied() > o  # type: ignore # noqa
-    __ge__ = lambda x, o: x._get_proxied() >= o  # type: ignore # noqa
-    __hash__ = lambda x: hash(x._get_proxied())  # type: ignore # noqa
-    __call__ = lambda x, *a, **kw: x._get_proxied()(*a, **kw)  # type: ignore # noqa
-    __len__ = lambda x: len(x._get_proxied())  # type: ignore # noqa
-    __getitem__ = lambda x, i: x._get_proxied()[i]  # type: ignore # noqa
-    __iter__ = lambda x: iter(x._get_proxied())  # type: ignore # noqa
-    __contains__ = lambda x, i: i in x._get_proxied()  # type: ignore # noqa
-    __add__ = lambda x, o: x._get_proxied() + o  # type: ignore # noqa
-    __sub__ = lambda x, o: x._get_proxied() - o  # type: ignore # noqa
-    __mul__ = lambda x, o: x._get_proxied() * o  # type: ignore # noqa
-    __floordiv__ = lambda x, o: x._get_proxied() // o  # type: ignore # noqa
-    __mod__ = lambda x, o: x._get_proxied() % o  # type: ignore # noqa
-    __divmod__ = lambda x, o: x._get_proxied().__divmod__(o)  # type: ignore # noqa
-    __pow__ = lambda x, o: x._get_proxied() ** o  # type: ignore # noqa
-    __lshift__ = lambda x, o: x._get_proxied() << o  # type: ignore # noqa
-    __rshift__ = lambda x, o: x._get_proxied() >> o  # type: ignore # noqa
-    __and__ = lambda x, o: x._get_proxied() & o  # type: ignore # noqa
-    __xor__ = lambda x, o: x._get_proxied() ^ o  # type: ignore # noqa
-    __or__ = lambda x, o: x._get_proxied() | o  # type: ignore # noqa
-    __div__ = lambda x, o: x._get_proxied().__div__(o)  # type: ignore # noqa
-    __truediv__ = lambda x, o: x._get_proxied().__truediv__(o)  # type: ignore # noqa
-    __neg__ = lambda x: -(x._get_proxied())  # type: ignore # noqa
-    __pos__ = lambda x: +(x._get_proxied())  # type: ignore # noqa
-    __abs__ = lambda x: abs(x._get_proxied())  # type: ignore # noqa
-    __invert__ = lambda x: ~(x._get_proxied())  # type: ignore # noqa
-    __complex__ = lambda x: complex(x._get_proxied())  # type: ignore # noqa
-    __int__ = lambda x: int(x._get_proxied())  # type: ignore # noqa
-    __float__ = lambda x: float(x._get_proxied())  # type: ignore # noqa
-    __oct__ = lambda x: oct(x._get_proxied())  # type: ignore # noqa
-    __hex__ = lambda x: hex(x._get_proxied())  # type: ignore # noqa
-    __index__ = lambda x: x._get_proxied().__index__()  # type: ignore # noqa
-    __coerce__ = lambda x, o: x._get_proxied().__coerce__(x, o)  # type: ignore # noqa
-    __enter__ = lambda x: x._get_proxied().__enter__()  # type: ignore # noqa
-    __exit__ = lambda x, *a, **kw: x._get_proxied().__exit__(*a, **kw)  # type: ignore # noqa
-    __radd__ = lambda x, o: o + x._get_proxied()  # type: ignore # noqa
-    __rsub__ = lambda x, o: o - x._get_proxied()  # type: ignore # noqa
-    __rmul__ = lambda x, o: o * x._get_proxied()  # type: ignore # noqa
-    __rdiv__ = lambda x, o: o / x._get_proxied()  # type: ignore # noqa
+    __setattr__ = lambda x, n, v: setattr(x._get_current_object(), n, v)  # type: ignore # noqa
+    __delattr__ = lambda x, n: delattr(x._get_current_object(), n)  # type: ignore # noqa
+    __str__ = lambda x: str(x._get_current_object())  # type: ignore # noqa
+    __lt__ = lambda x, o: x._get_current_object() < o  # type: ignore # noqa
+    __le__ = lambda x, o: x._get_current_object() <= o  # type: ignore # noqa
+    __eq__ = lambda x, o: x._get_current_object() == o  # type: ignore # noqa
+    __ne__ = lambda x, o: x._get_current_object() != o  # type: ignore # noqa
+    __gt__ = lambda x, o: x._get_current_object() > o  # type: ignore # noqa
+    __ge__ = lambda x, o: x._get_current_object() >= o  # type: ignore # noqa
+    __hash__ = lambda x: hash(x._get_current_object())  # type: ignore # noqa
+    __call__ = lambda x, *a, **kw: x._get_current_object()(*a, **kw)  # type: ignore # noqa
+    __len__ = lambda x: len(x._get_current_object())  # type: ignore # noqa
+    __getitem__ = lambda x, i: x._get_current_object()[i]  # type: ignore # noqa
+    __iter__ = lambda x: iter(x._get_current_object())  # type: ignore # noqa
+    __contains__ = lambda x, i: i in x._get_current_object()  # type: ignore # noqa
+    __add__ = lambda x, o: x._get_current_object() + o  # type: ignore # noqa
+    __sub__ = lambda x, o: x._get_current_object() - o  # type: ignore # noqa
+    __mul__ = lambda x, o: x._get_current_object() * o  # type: ignore # noqa
+    __floordiv__ = lambda x, o: x._get_current_object() // o  # type: ignore # noqa
+    __mod__ = lambda x, o: x._get_current_object() % o  # type: ignore # noqa
+    __divmod__ = lambda x, o: x._get_current_object().__divmod__(o)  # type: ignore # noqa
+    __pow__ = lambda x, o: x._get_current_object() ** o  # type: ignore # noqa
+    __lshift__ = lambda x, o: x._get_current_object() << o  # type: ignore # noqa
+    __rshift__ = lambda x, o: x._get_current_object() >> o  # type: ignore # noqa
+    __and__ = lambda x, o: x._get_current_object() & o  # type: ignore # noqa
+    __xor__ = lambda x, o: x._get_current_object() ^ o  # type: ignore # noqa
+    __or__ = lambda x, o: x._get_current_object() | o  # type: ignore # noqa
+    __div__ = lambda x, o: x._get_current_object().__div__(o)  # type: ignore # noqa
+    __truediv__ = lambda x, o: x._get_current_object().__truediv__(o)  # type: ignore # noqa
+    __neg__ = lambda x: -(x._get_current_object())  # type: ignore # noqa
+    __pos__ = lambda x: +(x._get_current_object())  # type: ignore # noqa
+    __abs__ = lambda x: abs(x._get_current_object())  # type: ignore # noqa
+    __invert__ = lambda x: ~(x._get_current_object())  # type: ignore # noqa
+    __complex__ = lambda x: complex(x._get_current_object())  # type: ignore # noqa
+    __int__ = lambda x: int(x._get_current_object())  # type: ignore # noqa
+    __float__ = lambda x: float(x._get_current_object())  # type: ignore # noqa
+    __oct__ = lambda x: oct(x._get_current_object())  # type: ignore # noqa
+    __hex__ = lambda x: hex(x._get_current_object())  # type: ignore # noqa
+    __index__ = lambda x: x._get_current_object().__index__()  # type: ignore # noqa
+    __coerce__ = lambda x, o: x._get_current_object().__coerce__(x, o)  # type: ignore # noqa
+    __enter__ = lambda x: x._get_current_object().__enter__()  # type: ignore # noqa
+    __exit__ = lambda x, *a, **kw: x._get_current_object().__exit__(*a, **kw)  # type: ignore # noqa
+    __radd__ = lambda x, o: o + x._get_current_object()  # type: ignore # noqa
+    __rsub__ = lambda x, o: o - x._get_current_object()  # type: ignore # noqa
+    __rmul__ = lambda x, o: o * x._get_current_object()  # type: ignore # noqa
+    __rdiv__ = lambda x, o: o / x._get_current_object()  # type: ignore # noqa
     __rtruediv__ = __rdiv__  # type: ignore # noqa
-    __rfloordiv__ = lambda x, o: o // x._get_proxied()  # type: ignore # noqa
-    __rmod__ = lambda x, o: o % x._get_proxied()  # type: ignore # noqa
-    __rdivmod__ = lambda x, o: x._get_proxied().__rdivmod__(o)  # type: ignore # noqa
-    __copy__ = lambda x: copy.copy(x._get_proxied())  # type: ignore # noqa
-    __deepcopy__ = lambda x, memo: copy.deepcopy(x._get_proxied(), memo)  # type: ignore # noqa
+    __rfloordiv__ = lambda x, o: o // x._get_current_object()  # type: ignore # noqa
+    __rmod__ = lambda x, o: o % x._get_current_object()  # type: ignore # noqa
+    __rdivmod__ = lambda x, o: x._get_current_object().__rdivmod__(o)  # type: ignore # noqa
+    __copy__ = lambda x: copy.copy(x._get_current_object())  # type: ignore # noqa
+    __deepcopy__ = lambda x, memo: copy.deepcopy(x._get_current_object(), memo)  # type: ignore # noqa
