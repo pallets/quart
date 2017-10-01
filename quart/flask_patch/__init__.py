4b34aa4f5d83d0c073553eb36af1b38683658786
pgjones <philip.graham.jones@googlemail.com>
2017-10-01 11:25:07 +0100

    Add missing API to the flask_patch system
    With an additional test to ensure the Quart API matches the patched
    Flask API. There are some additional aspects in the Flask API that
    shouldn't be present and require further investigation.

--- quart/flask_patch/__init__.py
+++ quart/flask_patch/__init__.py
@@ -33,16 +33,17 @@ from builtins import globals as builtin_globals  # noqa: E402
 
 from jinja2 import escape, Markup  # noqa: E402
 from quart import (
-    abort, appcontext_popped, appcontext_pushed, appcontext_tearing_down, before_render_template,
-    Blueprint, Config, got_request_exception, has_app_context, has_request_context, jsonify,
-    message_flashed, Quart, redirect, request_finished, request_started, request_tearing_down,
-    Response, ResponseReturnValue, template_rendered,
+    abort, after_this_request, appcontext_popped, appcontext_pushed, appcontext_tearing_down,
+    before_render_template, Blueprint, Config, got_request_exception, has_app_context,
+    has_request_context, jsonify, message_flashed, Quart, redirect, Request, request_finished,
+    request_started, request_tearing_down, Response, ResponseReturnValue, safe_join, send_file,
+    send_from_directory, template_rendered,
 )  # noqa: E402
 from quart.flask_patch.globals import (
     _app_ctx_stack, _request_ctx_stack, current_app, g, request, session,
 )  # noqa: E402
 from quart.flask_patch.helpers import make_response  # noqa: E402
-from quart.helpers import flash, get_flashed_messages, url_for  # noqa: E402
+from quart.helpers import flash, get_flashed_messages, get_template_attribute, url_for  # noqa: E402
 from quart.flask_patch.templating import render_template, render_template_string  # noqa: E402
 import quart.views  # noqa: E402,F401
 
@@ -114,11 +115,12 @@ from quart.local import LocalStack, TaskLocal  # noqa: E402
 LocalStack.__ident_func__ = lambda _: TaskLocal._task_identity()  # type: ignore
 
 __all__ = (
-    '_app_ctx_stack', '_request_ctx_stack', 'abort', 'appcontext_popped', 'appcontext_pushed',
-    'appcontext_tearing_down', 'before_render_template', 'Blueprint', 'Config', 'current_app',
-    'escape', 'flash', 'g', 'get_flashed_messages', 'got_request_exception', 'has_app_context',
-    'has_request_context', 'jsonify', 'Markup', 'make_response', 'message_flashed', 'Quart',
-    'redirect', 'render_template', 'render_template_string', 'request', 'request_finished',
-    'request_started', 'request_tearing_down', 'Response', 'ResponseReturnValue', 'session',
-    'template_rendered', 'url_for',
+    '_app_ctx_stack', '_request_ctx_stack', 'abort', 'after_this_request', 'appcontext_popped',
+    'appcontext_pushed', 'appcontext_tearing_down', 'before_render_template', 'Blueprint',
+    'Config', 'current_app', 'escape', 'flash', 'g', 'get_flashed_messages',
+    'got_request_exception', 'get_template_attribute', 'has_app_context', 'has_request_context',
+    'jsonify', 'Markup', 'make_response', 'message_flashed', 'Quart', 'redirect',
+    'render_template', 'render_template_string', 'request', 'request_finished', 'request_started',
+    'request_tearing_down', 'Request', 'Response', 'ResponseReturnValue', 'safe_join',
+    'send_file', 'send_from_directory', 'session', 'template_rendered', 'url_for',
 )
--- tests/test_flask_patch.py
+++ tests/test_flask_patch.py
@@ -1,12 +1,14 @@
 import quart.flask_patch  # noqa
 
+import flask
 import pytest
-from flask import Flask
+
+import quart
 
 
 @pytest.mark.asyncio
 async def test_flask_app() -> None:
-    app = Flask(__name__)
+    app = flask.Flask(__name__)
 
     @app.route('/')
     def index() -> str:
@@ -16,3 +18,14 @@ async def test_flask_app() -> None:
     response = await test_client.get('/')
     assert response.status_code == 200
     assert b'Hello' in (await response.get_data())
+
+
+def test_api_matches() -> None:
+    normalised_quart_api = set(dir(quart)) - set(['flask_patch'])
+    normalised_flask_api = set(dir(flask)) - set([
+        'Flask', 'flask_name', 'builtin_globals', 'new_handle_user_exception', 'name', 'module',
+        'quart', 'new_handle_http_exception', 'sys', 'LocalStack', 'old_handle_http_exception',
+        'asyncio', 'old_handle_user_exception', 'TaskLocal', 'flask_modules', 'HTTPException',
+        '_sync_wait',
+    ])
+    assert normalised_quart_api == normalised_flask_api
