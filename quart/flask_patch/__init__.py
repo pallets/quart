pallets/quart

commit e5a690d420f771492a3b54388caf537a4547b453

Bugfix await the get_data call
Commit b32548578980a0c1725058a280a1027456f811e0 changed the get_data
method on the Response to be async, this changed missed this call.
This also fixes, the until now mysterious, ``RuntimeWarning: coroutine
'Response.get_data' was never awaited`` error when testing.

quart/flask_patch/__init__.py
@@ -101,7 +101,7 @@ async def new_handle_http_exception(self, error: Exception) -> Response:  # type
         if handler is None:
             werkzeug_response = error.get_response()
             return await self.make_response((
-                werkzeug_response.get_data(), werkzeug_response.status_code,
+                (await werkzeug_response.get_data()), werkzeug_response.status_code,
                 werkzeug_response.headers,
             ))
         else:

commit 9a77f174dbbf757d91f243cc1f4370469221a2f7

Bugfix a get_data call
e5a690d420f771492a3b54388caf537a4547b453 introduced an attempt to fix
this, but in fact missed the actual issue which is that if the
Werkzeug HTTPException could not be imported it was set to be
``object``. This meant that all exceptions (as classes) were an
instance of this HTTPException and hence triggered the get_data
code. If the code was correct only the Werkzeug HTTPExceptions would
trigger the get_data call, and would not require an await.

quart/flask_patch/__init__.py
@@ -76,9 +76,10 @@ sys.modules.update(flask_modules)
 # HTTPExceptions to be considered in a special way (like the quart
 # HTTPException).
 try:
-    from werkzeug.exceptions import HTTPException
+    from werkzeug.exceptions import HTTPException  # type: ignore
 except ImportError:
-    HTTPException = object  # type: ignore
+    class HTTPException:  # type: ignore
+        pass
 
 
 old_handle_user_exception = Quart.handle_user_exception
@@ -101,7 +102,7 @@ async def new_handle_http_exception(self, error: Exception) -> Response:  # type
         if handler is None:
             werkzeug_response = error.get_response()
             return await self.make_response((
-                (await werkzeug_response.get_data()), werkzeug_response.status_code,
+                werkzeug_response.get_data(), werkzeug_response.status_code,
                 werkzeug_response.headers,
             ))
         else: