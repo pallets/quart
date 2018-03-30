43dd0d8405e8a00828643630e1779981356566d3
pgjones <philip.graham.jones@googlemail.com>
2018-03-30 23:52:44 +0100

    Test the HTTPServer drain system
    This tests the draining functionality introduced in
    cf82d539c67125eb3c3b96e2decb65966f6d174b.

--- tests/serving/test_base.py
+++ tests/serving/test_base.py
@@ -3,7 +3,28 @@ from unittest.mock import Mock
 
 import pytest
 
-from quart.serving._base import RequestResponseServer
+from quart.serving._base import HTTPServer, RequestResponseServer
+from .helpers import MockTransport
+
+
+@pytest.mark.asyncio
+async def test_http_server_drain(event_loop: asyncio.AbstractEventLoop) -> None:
+    transport = MockTransport()
+    server = HTTPServer(event_loop, transport, Mock(), '')  # type: ignore
+    server.pause_writing()
+
+    async def write() -> None:
+        server.write(b'Pre drain')
+        await server.drain()
+        server.write(b'Post drain')
+
+    asyncio.ensure_future(write())
+    await transport.updated.wait()
+    assert transport.data == b'Pre drain'
+    transport.clear()
+    server.resume_writing()
+    await transport.updated.wait()
+    assert transport.data == b'Post drain'
 
 
 @pytest.mark.asyncio
