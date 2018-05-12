13a65774c6801daaea558103656eb7651deceeaf
pgjones <philip.graham.jones@googlemail.com>
2018-05-12 14:59:03 +0100

    Improve the Flask Patching system to patch class members
    This should (I hope) make the system work with any Flask extension,
    however it is likely to have a fairly severe performance penalty
    compared with using Quart directly. It also figures out when code
    should be synchronous in a very hacky and unpleasant way.
    
    The Flask Patch system should now be used to help migrate code from
    Flask to Quart, rather than for any production traffic.

--- quart/flask_patch/_synchronise.py
+++ quart/flask_patch/_synchronise.py
@@ -1,5 +1,7 @@
 import asyncio
-from typing import Any, Awaitable
+import inspect
+import linecache
+from typing import Any, Awaitable, Callable
 
 from quart.globals import _app_ctx_stack, _request_ctx_stack, _websocket_ctx_stack
 
@@ -21,3 +23,36 @@ def sync_with_context(future: Awaitable) -> Any:
             return await future
 
     return asyncio.get_event_loop().sync_wait(context_wrapper())  # type: ignore
+
+
+def maybe_sync(func: Callable) -> Callable:
+    """Make the func synchronous iff it was called synchronously."""
+
+    def maybe_sync_wrapper(*args: Any, **kwargs: Any) -> Any:
+        try:
+            stack = inspect.stack()
+            if _is_async_call(func.__name__, stack[1].filename, stack[1].lineno):
+                return func(*args, **kwargs)
+            else:
+                return sync_with_context(func(*args, **kwargs))
+        finally:
+            del stack
+
+    return maybe_sync_wrapper
+
+
+def _is_async_call(func_name: str, filename: str, lineno: int, offset: int=0) -> bool:
+    """Try a best effort guess of how the function was called.
+
+    This is quite horrible to read/think about, but other attempts
+    using inspect are not affectual.
+    """
+    line = linecache.getline(filename, lineno - offset)
+    if 'await' in line or 'async' in line:
+        return True
+    elif f"{func_name}(" in line:
+        return False
+    elif offset < 100:  # Hard to imagine a function call with more than 100 lines...
+        return _is_async_call(func_name, filename, lineno, offset + 1)
+    else:
+        return False
