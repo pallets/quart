8062e61c281a8cc677d4493cda12306f046849ca
pgjones <philip.graham.jones@googlemail.com>
2018-05-28 20:37:05 +0100

    Change docs theme to the solar theme
    https://vimalkvn.github.io/solar-theme/index.html
    
    As it looks very nice and modern (to my taste).

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import solar_theme
+
+html_theme = 'solar_theme'
+html_theme_path = [solar_theme.theme_path]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
