f7083970700a4afe3c8248f6e61201693af7a529
pgjones <philip.graham.jones@googlemail.com>
2018-06-10 16:45:31 +0100

    Add date header testing
    This compliments be9f57ef04131499c3eef04106956d1bb9ad41b4 ensuring the
    functionality is tested.

--- tests/wrappers/test_response.py
+++ tests/wrappers/test_response.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timezone
+
+import hypothesis.strategies as strategies
 import pytest
+from hypothesis import given
 
 from quart.wrappers.response import Response
 
@@ -17,3 +21,17 @@ def test_response_cache_control() -> None:
     assert response.headers['Cache-Control'] == 'max-age=2'
     response.cache_control.no_cache = True
     assert response.headers['Cache-Control'] == 'max-age=2,no-cache'
+
+
+@given(
+    value=strategies.datetimes(
+        timezones=strategies.just(timezone.utc), min_value=datetime(1900, 1, 1),
+    ),
+)
+@pytest.mark.parametrize('header', ['date', 'expires', 'last_modified', 'retry_after'])
+def test_datetime_headers(header: str, value: datetime) -> None:
+    response = Response(b'Body')
+    value = value.replace(microsecond=0)
+    setattr(response, header, value)
+    assert response.headers.get(header.title().replace('_', '-'))
+    assert getattr(response, header) == value
