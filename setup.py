5466c615fe253faca408b805931dbfc8318062e1
pgjones <philip.graham.jones@googlemail.com>
2018-07-07 12:47:40 +0100

    Add Python 3.7 classifier to indicate support

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
