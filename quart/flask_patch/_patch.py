de39c6f6077c38c73a28fce81fb9f8bb6f621184
pgjones <philip.graham.jones@googlemail.com>
2018-07-13 20:20:06 +0100

    Bugfix ensure the Flask Patch system works with Python 3.7 & 3.6
    3.7 changes the way tasks are entered and left, introducing functions
    to do so.

--- quart/flask_patch/_patch.py
+++ quart/flask_patch/_patch.py
@@ -7,6 +7,15 @@ from typing import Any, Callable
 from quart.local import LocalStack, TaskLocal
 from ._synchronise import sync_with_context
 
+try:
+    from asyncio import _enter_task, _leave_task  # type: ignore
+except ImportError:
+    def _enter_task(loop: asyncio.AbstractEventLoop, task: asyncio.Task) -> None:
+        task.__class__._current_tasks[loop] = task  # type: ignore
+
+    def _leave_task(loop: asyncio.AbstractEventLoop, task: asyncio.Task) -> None:
+        task.__class__._current_tasks.pop(loop, task)  # type: ignore
+
 
 def _patch_asyncio() -> None:
     # This patches asyncio to add a sync_wait method to the event
@@ -22,15 +31,14 @@ def _patch_asyncio() -> None:
         self._ready.clear()
         future = asyncio.tasks.ensure_future(future, loop=self)
         preserved_task = future.current_task(self)
+        _leave_task(self, preserved_task)
         while not future.done() and not future.cancelled():
             self._run_once()
             if self._stopping:
                 break
         self._ready.extendleft(preserved_ready)
         if preserved_task is not None:
-            future.__class__._current_tasks[self] = preserved_task
-        else:
-            future.__class__._current_tasks.pop(self, None)
+            _enter_task(self, preserved_task)
         return future.result()
 
     asyncio.BaseEventLoop.sync_wait = _sync_wait  # type: ignore
