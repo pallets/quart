b4a4dd58964a40346756950274b1c0f547444823
pgjones <philip.graham.jones@googlemail.com>
2018-10-21 21:42:51 +0100

    Bugfix ensure the ASGI callable returns on completion
    Previously the callable would only return on receipt of a disconnect
    message, rather than returning either on a disconnect or when the
    handling is complete.
    
    Note that there is an intentional optimisation to ignore any
    disconnect messages and simply run the handling to completion in the
    case of http requests. This is to improve performance, which would
    suffer significantly with the multi task approach required for
    websockets.

--- tests/test_asgi.py
+++ tests/test_asgi.py
@@ -1,7 +1,9 @@
+import asyncio
+
 import pytest
 
 from quart import Quart
-from quart.asgi import ASGIHTTPConnection
+from quart.asgi import ASGIHTTPConnection, ASGIWebsocketConnection
 
 
 @pytest.mark.asyncio
@@ -20,5 +22,62 @@ async def test_http_1_0_host_header(headers: list, expected: str) -> None:
         'query_string': b'',
     }
     connection = ASGIHTTPConnection(app, scope)
-    connection._create_request_from_scope()
-    assert connection.request.headers['host'] == expected
+    request = connection._create_request_from_scope()
+    assert request.headers['host'] == expected
+
+
+@pytest.mark.asyncio
+async def test_http_completion() -> None:
+    # Ensure that the connecion callable returns on completion
+    app = Quart(__name__)
+    scope = {
+        'headers': [(b'host', b'quart')],
+        'http_version': '1.1',
+        'method': 'GET',
+        'scheme': 'https',
+        'path': '/',
+        'query_string': b'',
+    }
+    connection = ASGIHTTPConnection(app, scope)
+
+    queue: asyncio.Queue = asyncio.Queue()
+    queue.put_nowait({'type': 'http.request', 'body': b'', 'more_body': False})
+
+    async def receive() -> dict:
+        # This will block after returning the first and only entry
+        return await queue.get()
+
+    async def send(message: dict) -> None:
+        pass
+
+    # This test fails if a timeout error is raised here
+    await asyncio.wait_for(connection(receive, send), timeout=1)
+
+
+@pytest.mark.asyncio
+async def test_websocket_completion() -> None:
+    # Ensure that the connecion callable returns on completion
+    app = Quart(__name__)
+    scope = {
+        'headers': [(b'host', b'quart')],
+        'http_version': '1.1',
+        'method': 'GET',
+        'scheme': 'wss',
+        'path': '/',
+        'query_string': b'',
+        'extensions': {'websocket.http.response': {}},
+    }
+    connection = ASGIWebsocketConnection(app, scope)
+
+    queue: asyncio.Queue = asyncio.Queue()
+    queue.put_nowait({'type': 'websocket.connect'})
+
+    async def receive() -> dict:
+        # This will block after returning the first and only entry
+        return await queue.get()
+
+    async def send(message: dict) -> None:
+        pass
+
+    # This test fails if a timeout error is raised here
+    await asyncio.wait_for(connection(receive, send), timeout=1)
