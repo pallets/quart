pallets/quart

commit 0109e8665b42c059e44d84ddde3ab63c874671dd

Follow nest_asyncio technique for patching the task
This should make this part of the code more reliable and robust, as
nest_asyncio is better tested.

quart/flask_patch/_patch.py
@@ -26,22 +26,49 @@ def _patch_asyncio() -> None:
     if not isinstance(current_policy, target_policy):
         raise RuntimeError("Flask Patching only works with the default event loop policy")
 
+    _patch_loop()
+    _patch_task()
+
+
+def _patch_loop() -> None:
     def _sync_wait(self, future):  # type: ignore
         preserved_ready = list(self._ready)
         self._ready.clear()
         future = asyncio.tasks.ensure_future(future, loop=self)
-        preserved_task = future.current_task(self)
-        asyncio._leave_task(self, preserved_task)
         while not future.done() and not future.cancelled():
             self._run_once()
             if self._stopping:
                 break
         self._ready.extendleft(preserved_ready)
-        if preserved_task is not None:
-            asyncio._enter_task(self, preserved_task)
         return future.result()
 
     asyncio.BaseEventLoop.sync_wait = _sync_wait  # type: ignore
+
+
+def _patch_task() -> None:
+    # Patch the asyncio task to allow it to be re-entered.
+    def enter_task(loop, task):  # type: ignore
+        asyncio.tasks._current_tasks[loop] = task  # type: ignore
+
+    asyncio.tasks._enter_task = enter_task  # type: ignore
+
+    def leave_task(loop, task):  # type: ignore
+        del asyncio.tasks._current_tasks[loop]  # type: ignore
+
+    asyncio.tasks._leave_task = leave_task  # type: ignore
+
+    def step(self, exception):  # type: ignore
+        current_task = asyncio.tasks._current_tasks.get(self._loop)  # type: ignore
+        try:
+            self.__step_orig(self, exception)  # type: ignore
+        finally:
+            if current_task is None:
+                asyncio.tasks._current_tasks.pop(self._loop, None)  # type: ignore
+            else:
+                asyncio.tasks._current_tasks[self._loop] = current_task  # type: ignore
+
+    asyncio.Task._Task__step_orig = asyncio.Task._Task__step  # type: ignore
+    asyncio.Task._Task__step = step  # type: ignore
 
 
 def _context_decorator(func: Callable) -> Callable:

commit 93047a1ad3a9153140c9d11c617580d504597687

Bugfix Flask patching step definition
0109e8665b42c059e44d84ddde3ab63c874671dd broke the flask patching
system, this fixes it.

quart/flask_patch/_patch.py
@@ -57,10 +57,10 @@ def _patch_task() -> None:
 
     asyncio.tasks._leave_task = leave_task  # type: ignore
 
-    def step(self, exception):  # type: ignore
+    def step(self, exception=None):  # type: ignore
         current_task = asyncio.tasks._current_tasks.get(self._loop)  # type: ignore
         try:
-            self.__step_orig(self, exception)  # type: ignore
+            self._Task__step_orig(exception)  # type: ignore
         finally:
             if current_task is None:
                 asyncio.tasks._current_tasks.pop(self._loop, None)  # type: ignore