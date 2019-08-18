1595ed9ae6e882936d07ff51b876980fea3aa027
Giovanni Cappellotto <gcappellotto@fb.com>
2019-08-18 07:42:08 -0700

    Unblock logging I/O by logging in separate threads
    Create a new private function `_setup_logging_queue` that is used by
    both `create_logger` and `create_serving_logger`.
    
    The function creates a new instance of `LocalQueueHandler`, that is a
    slightly modified version of `QueueHandler` to avoid record preparation
    (no need for handling records within the same process). It starts a new
    `QueueListener` associated to the same queue, that will handle the
    logging I/O asynchronously.
    
    Note: two different `QueueListener` instances will handle logs
    respectively from the `quart.app` logger and the `quart.serving` logger.
    
    Resources:
    
    * https://www.zopatista.com/python/2019/05/11/asyncio-logging/
    * https://docs.python.org/3/howto/logging-cookbook.html
    * https://docs.python.org/3/library/logging.config.html
    * https://docs.python.org/3/library/logging.handlers.html

--- quart/logging.py
+++ quart/logging.py
@@ -1,5 +1,8 @@
 import sys
-from logging import DEBUG, Formatter, getLogger, INFO, Logger, NOTSET, StreamHandler
+from logging import DEBUG, Formatter, getLogger, Handler, INFO, Logger, LogRecord, NOTSET, \
+    StreamHandler
+from logging.handlers import QueueHandler, QueueListener
+from queue import SimpleQueue as Queue
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -10,6 +13,30 @@ default_handler.setFormatter(Formatter('[%(asctime)s] %(levelname)s in %(module)
 
 serving_handler = StreamHandler(sys.stdout)
 serving_handler.setFormatter(Formatter('[%(asctime)s] %(message)s'))
+
+
+class LocalQueueHandler(QueueHandler):
+    """Custom QueueHandler that skips record preparation.
+
+    There is no need to prepare records that go into a local, in-process queue,
+    we can skip that process and minimise the cost of logging further.
+    """
+    def prepare(self, record: LogRecord) -> LogRecord:
+        return record
+
+
+def _setup_logging_queue(*handlers: Handler) -> QueueHandler:
+    """Create a new LocalQueueHandler and start an associated QueueListener.
+    """
+    queue: Queue = Queue()
+    queue_handler = LocalQueueHandler(queue)
+
+    serving_listener = QueueListener(
+        queue, *handlers, respect_handler_level=True,
+    )
+    serving_listener.start()
+
+    return queue_handler
 
 
 def create_logger(app: 'Quart') -> Logger:
@@ -23,7 +50,8 @@ def create_logger(app: 'Quart') -> Logger:
     if app.debug and logger.level == NOTSET:
         logger.setLevel(DEBUG)
 
-    logger.addHandler(default_handler)
+    queue_handler = _setup_logging_queue(default_handler)
+    logger.addHandler(queue_handler)
     return logger
 
 
@@ -37,5 +65,6 @@ def create_serving_logger() -> Logger:
     if logger.level == NOTSET:
         logger.setLevel(INFO)
 
-    logger.addHandler(serving_handler)
+    queue_handler = _setup_logging_queue(serving_handler)
+    logger.addHandler(queue_handler)
     return logger
