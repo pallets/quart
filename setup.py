185859a0669a712c61be44c8dca191e0cf534258
pgjones <philip.graham.jones@googlemail.com>
2020-11-29 14:14:41 +0000

    Officially support Python 3.9
    By including it in the CI testing.

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
