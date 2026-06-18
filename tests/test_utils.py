bee238c59ee3bd870443e751ec52a93b6e019f56
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-06-18 08:42:35 +0200

    add a new test for synchronous iterables

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import pytest
 from werkzeug.datastructures import Headers
 
 from quart.utils import decode_headers
 from quart.utils import encode_headers
+from quart.utils import run_sync_iterable
 
 
 def test_encode_headers() -> None:
@@ -12,3 +14,11 @@ def test_encode_headers() -> None:
 
 def test_decode_headers() -> None:
     assert decode_headers([(b"foo", b"Bar")]) == Headers({"Foo": "Bar"})
+
+
+@pytest.mark.anyio
+async def test_run_sync_iterable() -> None:
+    def gen():
+        yield from range(4)
+
+    assert [v async for v in run_sync_iterable(gen())] == [0, 1, 2, 3]
